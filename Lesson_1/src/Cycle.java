50ca0a72b83cc0c77a6172efbf9001b32ab760a8
Force <daforsastudia@gmail.com>
2020-08-29 17:42:47 +0300

    HW 1

---
+++ Lesson_1/src/Cycle.java
@@ -0,0 +1,23 @@
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        int i = 6;
+        while (i > -7) {
+            System.out.println(i);
+            i -= 2;
+        }
+
+        i = 10;
+        int result = 0;
+        do {
+            if (i % 2 != 0) {
+                result += i;
+            }
+            i++;
+        } while (i < 20);
+        System.out.println(result);
+    }
+}
